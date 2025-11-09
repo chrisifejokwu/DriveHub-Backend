b231994d4be381de3fc48d14a2c51263e64712d4
chrisifejokwu <chrisifejokwu@gmail.com>
2025-11-09 17:01:09 +0000

    removed duplicate files

--- backend/src/models/CarDetail.ts
+++ backend/src/models/CarDetail.ts
@@ -6,7 +6,7 @@ export interface CarDetail {
     mileage: number;
     transmission: string;
     fuelType: string;
-    vin: string;
+ 
 }
 
 const carDetailSchema = new mongoose.Schema<CarDetail>({
@@ -16,9 +16,8 @@ const carDetailSchema = new mongoose.Schema<CarDetail>({
     mileage: { type: Number, required: true },
     transmission: { type: String, required: true },
     fuelType: { type: String, required: true },
-    vin: { type: String, required: true },
 });
-
+    
 const CarDetailModel = mongoose.model<CarDetail>('CarDetail', carDetailSchema);
 
 
